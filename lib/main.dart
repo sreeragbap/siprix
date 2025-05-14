c13f864d10e48d132dd2df7172bfecc0d8a5f4b1
sreeragiocod@git <sreerag@iocod.com>
2025-05-14 12:31:38 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:siprix_pro/app_widget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import siprix_voip_sdk_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SiprixVoipSdkPlugin.register(with: registry.registrar(forPlugin: "SiprixVoipSdkPlugin"))
+}
